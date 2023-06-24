1655a3e2ca9c71dc91e28ffc52df3dfc715a9c89
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-24 14:03:54 +0100

    crate hook for Toggle

---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+const useClickOutsideToggle = () => {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null);
+
+  /*
+    Handles user click outside of hamburger menu
+  */
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
+
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+
+  return { expanded, setExpanded, ref };
+};
+
+export default useClickOutsideToggle;
