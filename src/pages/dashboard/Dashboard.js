8ba08f6228c82ccac773efb0fd784c764517b9d2
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-24 14:23:06 +0100

    dashboard style is only bootstrap now

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -5,19 +5,10 @@ import { Line } from "react-chartjs-2";
 import TaskStatusTable from "../../components/TaskStatusTable";
 import TaskCompleteFilter from "../../components/TaskCompleteFilter";
 import HighPriorityTasks from "../../components/HighPriorityTasks";
-import styles from "../../styles/Dashboard.module.css";
-import appStyles from "../../App.module.css";
 
 const Dashboard = () => {
   const currentUser = useCurrentUser();
 
-  /*
-    Returns dashboard page with task count table,
-    line chart, high priority tasks &
-    not started tasks
-  */
-
-  // Example data for the line chart
   const lineChartData = {
     labels: ["January", "February", "March", "April", "May", "June"],
     datasets: [
@@ -32,54 +23,35 @@ const Dashboard = () => {
   };
 
   return (
-      <>
-        <Container className="mt-5">
-          <Row>
-            <Col className={`${appStyles.LightText} ${styles.BorderBox}`}>
-              <Col
-                  md={12}
-                  className={`${appStyles.LightText} ${styles.BorderBox} ${appStyles.Header}`}
-              >
-                Hello {currentUser.username} {currentUser.image}
-                <hr className={styles.HorizontalLine} />
-              </Col>
-              <Col
-                  md={12}
-                  className={`${appStyles.LightText} ${styles.BorderBox}`}
-              >
-                <Row>
-                  <Col>
-                    <TaskStatusTable />
-                  </Col>
-                  <Col className={`${appStyles.LightText} ${styles.BorderBox}`}>
-                    <Line data={lineChartData} />
-                  </Col>
-                </Row>
-                <hr className={styles.HorizontalLine} />
-              </Col>
-              <Col
-                  lg={12}
-                  className={`${appStyles.DarkText} ${styles.BorderBox} ${appStyles.TextAlignCenter}`}
-              >
-                <h3 className={`${appStyles.LightText} ${styles.BorderBox}`}>
-                  High Priority tasks
-                </h3>
-                <HighPriorityTasks />
-              </Col>
-              <Col
-                  lg={12}
-                  className={`${appStyles.DarkText} ${styles.BorderBox}  ${appStyles.TextAlignCenter}`}
-              >
-                <h3 className={`${appStyles.LightText} ${styles.BorderBox}`}>
-                  Not Started Tasks
-                </h3>
-                <TaskCompleteFilter />
-              </Col>
-              <div className={appStyles.ScrollToTopButton}></div>
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <Col md={12}>
+              Hello {currentUser.username} {currentUser.image}
+              <hr/>
             </Col>
-          </Row>
-        </Container>
-      </>
+            <Col md={12}>
+              <Row>
+                <Col>
+                  <TaskStatusTable />
+                </Col>
+                <Col>
+                  <Line data={lineChartData} />
+                </Col>
+              </Row>
+              <hr/>
+            </Col>
+            <Col lg={12}>
+              <h3>High Priority tasks</h3>
+              <HighPriorityTasks />
+            </Col>
+            <Col lg={12}>
+              <h3>Not Started Tasks</h3>
+              <TaskCompleteFilter />
+            </Col>
+          </Col>
+        </Row>
+      </Container>
   );
 };
 
