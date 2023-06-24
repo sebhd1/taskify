dc312f77d9ef8ba66902c1bcebe9d5b5a8ad0643
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-24 16:43:39 +0100

    add default bootstrap classes to HighPriorityTasks component

--- src/components/HighPriorityTasks.js
+++ src/components/HighPriorityTasks.js
@@ -4,7 +4,7 @@ import { axiosReq } from "../api/axiosDefaults";
 import Task from "../pages/tasks/Task";
 import Asset from "../components/Asset";
 import NoResults from "../assets/no-results.png";
-import appStyles from "../App.module.css";
+
 
 function HighPriorityTaskListings({ message, filter = "" }) {
   const [tasks, setTasks] = useState({ results: [] });
@@ -38,24 +38,23 @@ function HighPriorityTaskListings({ message, filter = "" }) {
   */
   return (
     <Row
-      className={`${appStyles.JustifyContentCenter} ${appStyles.BottomMargin}`}
     >
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         {hasLoaded ? (
-          <div className={appStyles.ScrollBox}>
+          <div>
             {tasks.results.length ? (
               tasks.results.map((tasks) => (
                 <Task key={tasks.id} {...tasks} setTasks={setTasks} />
               ))
             ) : (
-              <Container className={appStyles.Content}>
+              <Container>
                 <Asset src={NoResults} message={message} />
                 <p>It seems all tasks have been completed!</p>
               </Container>
             )}
           </div>
         ) : (
-          <Container className={appStyles.Content}>
+          <Container>
             <Asset spinner />
           </Container>
         )}
