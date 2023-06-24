7aeefda49a7c8affce79bfb6124e259884da05b2
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-24 14:20:25 +0100

    create ProfileData context to handle profile request data

---
+++ src/contexts/ProfileDataContext.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+
+const ProfileDataContext = createContext();
+const SetProfileDataContext = createContext();
+export const useProfileData = () => useContext(ProfileDataContext);
+export const useSetProfileData = () => useContext(SetProfileDataContext);
+
+export const ProfileDataProvider = ({ children }) => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    listProfiles: { results: [] },
+  });
+
+  const currentUser = useCurrentUser();
+
+  /*
+    Makes API request to the /profiles/ endpoint
+    Sends profile information
+    Updates profile page and listProfiles data
+  */
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=tasks_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          listProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <ProfileDataContext.Provider value={profileData}>
+      <SetProfileDataContext.Provider value={setProfileData}>
+        {children}
+      </SetProfileDataContext.Provider>
+    </ProfileDataContext.Provider>
+  );
+};
