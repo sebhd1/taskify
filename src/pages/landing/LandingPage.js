dd00cd6642d549ae50b6ec5bb7ba26ce10f28c01
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-24 14:21:34 +0100

    improve landing page style and added some context

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Card, Button, Row, Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import styles from "../../styles/LandingPage.module.css";
-import appStyles from "../../App.module.css";
 import landingImage from "../../assets/landing-image.jpg";
+import taskImage from '../../assets/task-management.png';
 
 const LandingPage = () => {
     return (
@@ -28,15 +27,14 @@ const LandingPage = () => {
                         <Card.Body>
                             <div className="d-flex justify-content-around">
                                 <Link to="/signin">
-                                    <Button
-                                        className={`btn ${appStyles.Button} mb-3`}
+                                    <Button className='mb-3'
                                     >
                                         Log in
                                     </Button>
                                 </Link>
                                 <Link to="/signup">
                                     <Button
-                                        className={`btn ${appStyles.Button} mb-3`}
+                                        className='mb-3'
                                     >
                                         Sign up
                                     </Button>
@@ -49,11 +47,11 @@ const LandingPage = () => {
                     <img
                         src={landingImage}
                         alt="Taskify landing"
-                        className={`img-fluid ${styles.LandingImage}`}
+                        className='img-fluid'
                     />
                 </Col>
             </Row>
-            <Container className={styles.FooterWidth}></Container>
+            <Container></Container>
             <Row className="mt-5">
                 <Col md={6}>
                     <h2>Features</h2>
@@ -65,16 +63,50 @@ const LandingPage = () => {
                         <li>Receive notifications and reminders for upcoming deadlines</li>
                         <li>Generate reports and visualize task statistics</li>
                     </ul>
+                    <img src={taskImage} alt="taskImage" className='img-fluid' />
                 </Col>
                 <Col md={6}>
-                    <h2>Get Started</h2>
-                    <p>
-                        Sign up for an account to start managing your tasks with Taskify. It's
-                        easy and free!
-                    </p>
-                    <Button variant="primary" href="/signup">
-                        Sign Up
-                    </Button>
+                    <section id="how-it-works" className="bg-light py-5">
+                        <div className="container">
+                            <h2 className="text-center mb-4">How It Works</h2>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h4>Create Tasks</h4>
+                                    <p>Create tasks and assign them to team members. Set due dates and add relevant
+                                        details.</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <h4>Collaborate</h4>
+                                    <p>Collaborate with your team members by commenting on tasks and sharing
+                                        updates.</p>
+                                </div>
+                            </div>
+                            <div className="row mt-4">
+                                <div className="col-md-6">
+                                    <h4>Track Progress</h4>
+                                    <p>Track task progress and receive notifications to ensure tasks are completed on
+                                        time.</p>
+                                </div>
+                                <div className="col-md-6">
+                                    <h4>Monitor Performance</h4>
+                                    <p>Analyze team performance with built-in analytics and improve productivity.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <div className="mt-5">
+                        <h2>Get Started</h2>
+                        <p>
+                            Sign up for an account to start managing your tasks with Taskify. It's
+                            easy and free!
+                        </p>
+
+                        <Button variant="primary" href="/signup" className="mb-5">
+                            Sign Up
+                        </Button>
+                    </div>
+
                 </Col>
             </Row>
         </Container>
