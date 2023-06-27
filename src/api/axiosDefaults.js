0a26f5d82c7e5990083271613c40c069a09a85e6
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-27 18:11:42 +0100

    update axios default URL with deployed app to test

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // unique URL for deployed API project on Heroku
-axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+axios.defaults.baseURL = 'https://taskify-api-c1fb8f20846a.herokuapp.com/'
 // data format by API
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 // CORS errors avoidance when sending cookies
