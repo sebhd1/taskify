840ccda08455e41200beae1d8247873e44f728ae
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-27 18:12:04 +0100

    removed chart for debugging purpose

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Line } from "react-chartjs-2";
 import TaskStatusTable from "../../components/TaskStatusTable";
 import TaskCompleteFilter from "../../components/TaskCompleteFilter";
 import HighPriorityTasks from "../../components/HighPriorityTasks";
@@ -9,18 +8,6 @@ import HighPriorityTasks from "../../components/HighPriorityTasks";
 const Dashboard = () => {
   const currentUser = useCurrentUser();
 
-  const lineChartData = {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [
-      {
-        label: "Task Progress",
-        data: [65, 59, 80, 81, 56, 55],
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        tension: 0.1,
-      },
-    ],
-  };
 
   return (
       <Container className="mt-5">
@@ -34,9 +21,6 @@ const Dashboard = () => {
               <Row>
                 <Col>
                   <TaskStatusTable />
-                </Col>
-                <Col>
-                  <Line data={lineChartData} />
                 </Col>
               </Row>
               <hr/>
