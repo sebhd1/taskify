9c5f1cdf475e7de7fff7089e4d1277367ccfddb6
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-06-27 14:20:15 +0100

    Setup Avatar.js component for user avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../styles/Avatar.module.css";
+
 
 /*
   Component used for  displaying a users avatar 
@@ -9,7 +9,6 @@ const Avatar = ({ src, height = 45, text }) => {
   return (
     <span>
       <img
-        className={styles.Avatar}
         src={src}
         height={height}
         width={height}
